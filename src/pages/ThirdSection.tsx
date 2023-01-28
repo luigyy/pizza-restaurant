2e09e7c3d1be3036d7f13a4e6449e62026d5f33c
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-27 21:28:47 -0600

    finish third section

--- src/pages/ThirdSection.tsx
+++ src/pages/ThirdSection.tsx
@@ -4,16 +4,31 @@ interface ThirdSectionProps {}
 
 const ThirdSection: React.FC<ThirdSectionProps> = ({}) => {
   return (
-    <div className="h-screen border-2 border-blue-800">
-      <h1 className="border-2 border-red-500 max-w-[45%] mx-auto pt-32 font-[Gazeta] text-center text-4xl font-medium text-zinc-900">
+    <div className="h-[110vh] border-2 border-blue-800">
+      <h1 className="max-w-[45%] mx-auto pt-28 font-[Gazeta] text-center text-4xl font-medium text-zinc-900">
         Lorem ipsum dolor sit amet . dar Etienne{" "}
         <span className="text-[#827c29]">consectecteur</span> mollitia .
       </h1>
       {/* photos div */}
-      <div className="flex w-full border-8 justify-center">
-        <div className="border-8 w-[25%] h-1/2"></div>
-        <div className="border-8 w-[25%] h-1/2"></div>
-        <div className="border-8 w-[25%] h-1/2"></div>
+      <div className="flex w-full h-[40%]  justify-center pt-10">
+        <div className="w-[25%] h-full ">
+          <div className=" w-full h-full bg-[url('ingredients.jpg')] bg-cover"></div>
+          <h1 className="text-center pt-10 text-lg text-zinc-700/90">
+            Lorem, ipsum dolor.
+          </h1>
+        </div>
+        <div className="w-[25%] h-full ">
+          <div className=" w-full h-full bg-[url('chef.jpg')] bg-cover"></div>
+          <h1 className="text-center pt-10 text-lg text-zinc-700/90">
+            Lorem, ipsum dolor.
+          </h1>
+        </div>
+        <div className="w-[25%] h-full ">
+          <div className=" w-full h-full bg-[url('pizza-oven.jpg')] bg-cover"></div>
+          <h1 className="text-center pt-10 text-lg text-zinc-800/90">
+            Lorem, ipsum dolor.
+          </h1>
+        </div>
       </div>
     </div>
   );
