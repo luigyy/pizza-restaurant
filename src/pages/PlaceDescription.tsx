94818b17ffcc638ca5be5b44f3027e8b9f0b3d12
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-28 23:31:29 -0600

    finish restaurant section

--- src/pages/PlaceDescription.tsx
+++ src/pages/PlaceDescription.tsx
@@ -23,7 +23,7 @@ const PlaceDescription: React.FC<PlaceDescriptionProps> = ({}) => {
             alt=""
           />
           <img src="restaurant.jpg" className="w-1/4 right-0 absolute" alt="" />
-          <h1 className="text-4xl">Lorem, ipsum dolor.</h1>
+          <h1 className="text-4xl font-[Gazeta]">Lorem, ipsum dolor.</h1>
           <p className="pt-7 text-sm font-display text-zinc-700">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
             explicabo harum repudiandae aut consectetur dolorum, dolore vero
@@ -37,6 +37,25 @@ const PlaceDescription: React.FC<PlaceDescriptionProps> = ({}) => {
             aliquid ullam. Excepturi fugiat molestiae perspiciatis nulla dolorum
             veniam. Sed nam quisquam id culpa sequi suscipit, dolore officiis
             harum. Amet temporibus atque inventore!
+          </p>
+          <div className="pt-3">
+            <input
+              checked={true}
+              type="radio"
+              className="mr-3 appearance-none  w-[14px] h-[14px] rounded-full border outline-none border-[#817f5d] checked:bg-[#dbcc91]"
+            />
+            <input type="radio" className="mr-3" />
+            <input type="radio" className="mr-3" />
+          </div>
+        </div>
+        <div className="pt-16 pl-36">
+          <h1 className="text-3xl font-[Gazeta]">
+            <span className="text-[#7b6b20]"> Lorem ipsum</span> dolor sit
+            adipisicing{" "}
+          </h1>
+          <p className="text-display opacity-80 text-2xl">+123453533</p>
+          <p className="text-sm font-display opacity-80">
+            ...Lorem ipsum dolor sit amet.
           </p>
         </div>
       </div>
