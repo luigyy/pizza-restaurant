e41bb2eaa56fb1d3c0b883568279c781e94e43f6
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-29 19:45:51 -0600

    fixed center alignment in hero of both fork img and pasta title

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "../Hero.css";
 
 interface HeroProps {}
 
 const Hero: React.FC<HeroProps> = ({}) => {
   return (
-    <div className="bg-white h-[110vh]  ">
+    <div className="bg-white h-[110vh] w-full ">
       <Navbar />
       <img
         src="fork.png"
         alt=""
-        className="md:absolute md:w-[500px] md:right-[32%] max-w-[500px] w-3/4 "
+        className="md: child md:w-[500px] md: max-w-[500px] w-3/4 mx-auto "
       />
-      <h1 className="md:text-zinc-900 md:font-[Gazeta] md:font-bold md:text-[175px] md:py-[26vh]  md:text-center md:tracking-[0.4em]  ">
+      <h1 className="md:text-zinc-900 md:font-[Gazeta] md:font-bold md:text-[175px] md:py-[26vh]  md:text-center md:tracking-[0.4em] md:pl-10  ">
         PASTA
       </h1>
     </div>
