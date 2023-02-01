5b95c2eeb45a50d21e0db5499209a4c2cf3c880a
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-31 19:00:26 -0600

    change fork heigh for mobile devices

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: React.FC<HeroProps> = ({}) => {
       <img
         src="fork.png"
         alt=""
-        className="md: child md:w-[500px] md:max-w-[500px] sm:max-w-[350px] max-w-[275px] w-3/4 mx-auto "
+        className="md: child md:w-[500px] md:max-w-[500px] sm:max-w-[350px] max-w-[300px] w-3/4 mx-auto "
       />
       <h1 className="md:text-zinc-900 font-[Gazeta] md:text-[175px] md:py-[26vh]  text-center md:tracking-[0.4em] md:pl-10 py-56 text-8xl sm:text-9xl  ">
         PASTA
