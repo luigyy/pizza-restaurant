b872464f1c5508fadaa5b2504ce62542ca71e11e
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-31 18:50:23 -0600

    added responsiveness to fork in hero

--- src/pages/SecondSection.tsx
+++ src/pages/SecondSection.tsx
@@ -4,10 +4,10 @@ interface SecondProps {}
 
 const Second: React.FC<SecondProps> = ({}) => {
   return (
-    <div className="h-[80vh] w-full  bg-[#363e26] flex">
+    <div className="min-h-[80vh] w-full  bg-[#363e26] flex">
       {/* left div  */}
-      <div className="h-full w-1/2 font-display">
-        <h1 className="max-w-[70%] text-3xl font-extrabold text-[#f8f7f7]/90 tracking-wide font-[Gazeta] px-20 pt-20">
+      <div className="h-full md:w-1/2  w-full  font-display">
+        <h1 className="md:max-w-[70%] text-center  md:text-left text-3xl font-extrabold text-[#f8f7f7]/90 tracking-wide font-[Gazeta] px-20 pt-20">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </h1>
         <p className="px-20  max-w-[70%] pt-6 text-[#f8f7f7]/70">
@@ -48,7 +48,7 @@ const Second: React.FC<SecondProps> = ({}) => {
         </ul>
       </div>
       {/* right div  */}
-      <div className="relative h-full w-1/2 ">
+      <div className="hidden md:inline relative h-full w-1/2 ">
         <div className="absolute w-full border-2h-full right-5 -top-10">
           <img src="mountain.jpg" alt="" />
         </div>
--- src/pages/ThirdSection.tsx
+++ src/pages/ThirdSection.tsx
@@ -4,28 +4,28 @@ interface ThirdSectionProps {}
 
 const ThirdSection: React.FC<ThirdSectionProps> = ({}) => {
   return (
-    <div className="h-[110vh] ">
+    <div className="md:h-[110vh] h-[180vh] md:mb-0 mb-96 ">
       <h1 className="max-w-[45%] mx-auto pt-28 font-[Gazeta] text-center text-4xl font-medium text-zinc-900">
         Lorem ipsum dolor sit amet . dar Etienne{" "}
         <span className="text-[#827c29]">consectecteur</span> mollitia .
       </h1>
       {/* photos div */}
-      <div className="flex w-full h-[40%]  justify-center pt-10">
-        <div className="w-[25%] h-full ">
-          <div className=" w-full h-full bg-[url('ingredients.jpg')] bg-cover"></div>
+      <div className="flex md:flex-row flex-col md:gap-0 gap-32 w-full md:h-[40%] h-full justify-center pt-10">
+        <div className="md:w-[25%] w-1/2 md:mx-0 mx-auto   h-full ">
+          <div className=" w-full h-full bg-[url('ingredients.jpg')] bg-cover bg-center"></div>
           <h1 className="text-center pt-10 text-lg text-zinc-700/90">
             Lorem, ipsum dolor.
           </h1>
         </div>
-        <div className="w-[25%] h-full ">
-          <div className=" w-full h-full bg-[url('chef.jpg')] bg-cover"></div>
+        <div className="md:w-[25%] w-1/2 md:mx-0 mx-auto  h-full  ">
+          <div className=" w-full h-full bg-[url('chef.jpg')] bg-cover bg-center"></div>
           <h1 className="text-center pt-10 text-lg text-zinc-700/90">
             Lorem, ipsum dolor.
           </h1>
         </div>
-        <div className="w-[25%] h-full ">
-          <div className=" w-full h-full bg-[url('pizza-oven.jpg')] bg-cover"></div>
-          <h1 className="text-center pt-10 text-lg text-zinc-800/90">
+        <div className="md:w-[25%] w-1/2 md:mx-0 mx-auto  h-full ">
+          <div className=" w-full h-full bg-[url('pizza-oven.jpg')] bg-cover bg-center"></div>
+          <h1 className="text-center pt-10 text-lg text-zinc-700/90">
             Lorem, ipsum dolor.
           </h1>
         </div>
