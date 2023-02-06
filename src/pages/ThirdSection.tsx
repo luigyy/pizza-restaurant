9d793ba3524e305a467a05c3249f7ca14bf7cb6d
luigyy <luigy.valverde@ucr.ac.cr>
2023-02-06 17:06:05 -0600

    change img from bg image to img tag in third section

--- src/pages/ThirdSection.tsx
+++ src/pages/ThirdSection.tsx
@@ -12,19 +12,38 @@ const ThirdSection: React.FC<ThirdSectionProps> = ({}) => {
       {/* photos div */}
       <div className="flex md:flex-row flex-col w-full md:gap-0 gap-32 md:h-[40%] h-full  justify-center pt-10">
         <div className="md:w-[25%] w-[70%] h-full md:mx-0 mx-auto">
-          <div className=" w-full h-full bg-[url('ingredients.jpg')] bg-cover bg-center "></div>
+          <div className=" w-full h-full  ">
+            <img
+              src="ingredients.jpg"
+              className="w-full h-full object-cover object-center"
+              alt=""
+            />
+          </div>
           <h1 className="text-center pt-10 text-lg text-zinc-700/90">
             Lorem, ipsum dolor.
           </h1>
         </div>
         <div className="md:w-[25%] w-[70%] h-full md:mx-0 mx-auto">
-          <div className=" w-full h-full bg-[url('chef.jpg')] bg-cover bg-center "></div>
+          <div className=" w-full h-full bg-[url('chef.jpg')] bg-cover bg-center ">
+            <img
+              src="chef.jpg"
+              className="object-cover object-center w-full h-full"
+              alt=""
+            />
+          </div>
+
           <h1 className="text-center pt-10 text-lg text-zinc-700/90">
             Lorem, ipsum dolor.
           </h1>
         </div>
         <div className="md:w-[25%] w-[70%] h-full md:mx-0 mx-auto">
-          <div className=" w-full h-full bg-[url('pizza-oven.jpg')] bg-cover bg-center "></div>
+          <div className=" w-full h-full ">
+            <img
+              src="pizza-oven.jpg"
+              className="object-cover object-center w-full h-full"
+              alt=""
+            />
+          </div>
           <h1 className="text-center pt-10 text-lg text-zinc-800/90">
             Lorem, ipsum dolor.
           </h1>
